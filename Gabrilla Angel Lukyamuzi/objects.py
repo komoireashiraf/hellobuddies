b870dc1c3463ed5c2488c4648808267ba279af8e
Gabriella Angel🦋 <g.bayinze004@gmail.com>
2025-09-18 20:29:27 +0300

    Add files via upload

---
+++ Gabrilla Angel Lukyamuzi/objects.py
@@ -0,0 +1,63 @@
+##Objects and their Instances
+##1.bag
+#class = "accessory"
+
+##Instances
+#clutch, black
+#purse, pink 
+
+def_init_(self, type, colour):
+   self.type = type
+   self.colour = colour
+
+
+##2. soap
+#class = toiletry
+
+##instances
+#pink, bathing, tablet
+#white, washing, bar
+
+def_init_(self, colour, type, size):
+   self.colour = colour
+   self.type = type
+   self.size = size
+
+##3. fish
+#class = "animal"
+
+##instances
+#black, lungfish, freshwater
+#orange, goldfish, temperate water
+#gray, shark, ocean
+
+def_init_(self, colour, type, habitant)
+   self.colour = colour
+   self.type = type
+   self.habitant = habitant
+
+##4.phone
+#class = "device"
+
+##instances
+#apple, titanium, 1TB
+#samsung, icy blue mint, 256GB
+#huawei, vibrant Aurora, 512GB
+
+def_init_(self, brand, colour, storage)
+   self.brand = brand 
+   self.colour = colour
+   self.storage = storage
+
+
+##5.refrigerator
+#class = "appliance"
+
+##instances
+#hisense, silver, double door
+#LG, white, single door
+
+def_init_(self, brand, colour, type)
+   self.brand = brand
+   self.colour = colour
+   self. type = type
