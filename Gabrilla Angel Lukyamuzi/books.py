029a8de7bbe8df4a01f2cc7f32798f42e8dbb0e8
Gabriella Angel🦋 <g.bayinze004@gmail.com>
2025-09-18 20:34:15 +0300

    Add files via upload

---
+++ Gabrilla Angel Lukyamuzi/books.py
@@ -0,0 +1,17 @@
+##defining the class
+class Book:
+    def __init__(self, title, author, year):
+       self.title = title
+       self.author = author
+       self.year = year
+
+    def describe(self):
+        return f"{self.title} by {self.author} in {self.year}."
+
+##the books
+book1 = Book("Twilight Book 1", "Stephenie Meyer", 2005)
+book2 = Book("The Fault in our Stars", "John Green", 2012)
+
+##calling the books
+print(book1.describe())
+print(book2.describe())
