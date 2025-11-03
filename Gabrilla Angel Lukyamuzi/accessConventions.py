46035842eb520b1f87d78c79c90a7fbd8d01f5d0
Gabriella Angel🦋 <g.bayinze004@gmail.com>
2025-11-03 20:58:24 +0300

    Update accessConventions.py

--- Gabrilla Angel Lukyamuzi/accessConventions.py
+++ Gabrilla Angel Lukyamuzi/accessConventions.py
@@ -3,7 +3,7 @@ class Student:
     def __init__(self, name):
         self.name = name             ##public
         self._gpa = 3.5              ##protected
-        self.__password = "1234"        ##private
+        self.__password = "8890."        ##private
 
 s1 = Student('Gabriella')
 print(f"My name is {s1.name}. I have a GPA of {s1._gpa}.")   ##cannot print password because it was made private
@@ -11,7 +11,8 @@ print(f"My name is {s1.name}. I have a GPA of {s1._gpa}.")   ##cannot print pass
 ##changing student name and gpa and password
 s1.name = 'Jennifer'
 s1._gpa = 2.9
-s1.__password = "8970"
+s1.__password = "345£"
 
 print(s1.name)
 print(s1._gpa)
+
